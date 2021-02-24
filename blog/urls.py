da135c2552336259524d12259ed756351995f463
Yk <kone2199@gmail.com>
2021-02-24 11:30:37 +0000

    configurations des routes

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from .import views
 
 urlpatterns = [
    path('', views.home, name="home"),
+   path('blog', views.blog, name="blog"),
+   path('detail', views.detail, name="detail"),
+   path('projet', views.projetDetail, name="projetDetail"),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,29 @@ def home(request):
 
     }
     return render(request, 'index.html', context)
+
+
+def blog(request):
+    context={
+
+    }
+
+    return render(request, 'blog.html', context)
+
+
+def detail(request):
+    context = {
+
+    }
+
+    return render(request, 'blog-detail.html', context)
+
+
+
+def projetDetail(request):
+
+    context = {
+
+    }
+
+    return render(request, 'projet-detail.html', context)
